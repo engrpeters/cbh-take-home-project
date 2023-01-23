83b155626570c97d6fded12f47f0170a37d4dcaa
Oluwafemi Peters <femipeters08@yahoo.com>
2023-01-23 13:17:04 +0000

    Update dpk.test.js
    updated test for double hash function

--- dpk.test.js
+++ dpk.test.js
@@ -1,12 +1,12 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
-
-
+  
   it('should return the partition key if it exists', () => {
     expect(deterministicPartitionKey({ partitionKey: 'test' })).toEqual('test');
   });
@@ -17,9 +17,10 @@ describe("deterministicPartitionKey", () => {
     expect(deterministicPartitionKey(event)).toEqual(hash);
   });
 
-  it('should return the SHA3-512 hash of the event if the partition key is longer than 256 characters', () => {
-    const event = { partitionKey: 'a'.repeat(257) };
-    const hash = crypto.createHash("sha3-512").update(event.partitionKey).digest("hex");
+  it('should return the SHA3-512 hash of the partition key if it longer than 256 characters', () => {
+    const partitionKey = 'a'.repeat(257);
+    const event = { partitionKey };
+    const hash = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
     expect(deterministicPartitionKey(event)).toEqual(hash);
   });
 
